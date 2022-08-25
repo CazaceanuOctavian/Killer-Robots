ce951080306924c5901ed5e24d463dd9a7a2f66b
Cazaceanu Octavian <o.cazatavian@gmail.com>
2022-08-25 14:43:53 +0300

    added constructors for normal/killer robots + debugging

--- src/killer_robots/robot.java
+++ src/killer_robots/robot.java
@@ -18,16 +18,96 @@ public class robot {
 	boolean alive;
 	
 	
-	public robot (String name, int robotHp, int posX, int posY){
-		
+	public robot (String robotName, int robotHp, int posX, int posY, int viewRange){
+		this.robotName=robotName;
+		this.robotHp=robotHp;
+		this.posX=posX;
+		this.posY=posY;
+		this.viewRange=viewRange;
+		this.killerRobot=true;
+		this.alive=true;
 	}
 	
+	public robot (String robotName, int robotHp, int posX, int posY){
+		this.robotName=robotName;
+		this.robotHp=robotHp;
+		this.posX=posX;
+		this.posY=posY;
+		this.alive=true;
+	}
+	
+	public void setName(String robotName) {
+		this.robotName=robotName;
+	}
+	
+	public void setRobotHp(int robotHP) {
+		this.robotHp=robotHp;
+	}
+	
+	public void setPosX(int posX) {
+		this.posX=posX;
+	}
+	
+	public void setPosY(int posY) {
+		this.posY=posY;
+	}
+	
+	public void setViewRange(int viewRange) {
+		this.viewRange=viewRange;
+	}
+	
+	public void setKillerRobot(boolean killerRobot) {
+		this.killerRobot=killerRobot;
+	}
+	
+	public void setAliveStatus(boolean alive) {
+		this.alive=alive;
+	}
+	
+	
+	public String getName(){
+		return this.robotName;
+	}
+	
+	public int getRobotHp() {
+		return this.robotHp;
+	}
+	
+	public int getPosX() {
+		return this.posX;
+	}
+	
+	public int getPosY() {
+		return this.posY;
+	}
+	
+	public int getViewRange() {
+		return this.viewRange;
+	}
+	
+	public boolean getKillerRobot() {
+		return this.killerRobot;
+	}
+	
+	public boolean getAliveStatus() {
+		return this.alive;
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		List<robot> arrayList = new ArrayList<>(5);
+		robot killerRobot = new robot("roby", 100, 1, 2, 15);
 		
+		killerRobot.setKillerRobot(true);
+		System.out.println(killerRobot.getKillerRobot());
+		System.out.println(killerRobot.getViewRange());
 		
+		robot normalRobot = new robot("robustus", 100, 5, 3);
 		
+		System.out.println(normalRobot.getKillerRobot());
+		System.out.println(normalRobot.getViewRange());
+		System.out.println(normalRobot.getAliveStatus());
 		
-	input.close();
+		input.close();
 	}
 }
