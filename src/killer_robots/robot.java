23a79a82fc950dfad605afc40e85abb5f7a329e7
Cazaceanu Octavian <o.cazatavian@gmail.com>
2022-08-25 14:04:39 +0300

    added robot class

---
+++ src/killer_robots/robot.java
@@ -0,0 +1,33 @@
+package killer_robots;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class robot {
+	
+	static Scanner input= new Scanner(System.in);
+	
+	String robotName;
+	int robotHp;
+	int posX;
+	int posY;
+	int viewRange;
+	boolean killerRobot;
+	boolean alive;
+	
+	
+	public robot (String name, int robotHp, int posX, int posY){
+		
+	}
+	
+	public static void main(String[] args) {
+		List<robot> arrayList = new ArrayList<>(5);
+		
+		
+		
+		
+	input.close();
+	}
+}
